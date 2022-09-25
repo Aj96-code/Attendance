fb2210b28433bb26a81e64f68595c713001e305d
Aj96-code <69065112+Aj96-code@users.noreply.github.com>
2022-09-25 01:20:35 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,5 +1,5 @@
 <?php
-   /* //dev connection
+    //dev connection
     $host = "127.0.0.1";
     $db = "attendance";
     $user = "root";
@@ -7,8 +7,8 @@
     $charset = "utf8mb4";
     //* data source name
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    */
     
+    /*
     //remote connection
     $host ="sql5.freesqldatabase.com";
     $db = "sql5521882";
@@ -16,7 +16,7 @@
     $password = "RaLPldAeMf";
     $charset = "utf8mb4";
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    try
+    */try
     {
         $pdo = new PDO($dsn,$user,$password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
